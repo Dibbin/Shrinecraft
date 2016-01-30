638f85cdfa7473b97fcc93cb31d73861cf1d02f2
uberblah <miles.rufatlatre@colorado.edu>
2016-01-30 12:21:37 -0700

    added cursorcast utility, will use it for listener

---
+++ Assets/Scripts/CursorCaster.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class CursorCaster {
+    public class CursorCastHit
+    {
+        public GameObject target = null;
+        public Vector3 worldPosition = new Vector3();
+
+        public CursorCastHit(GameObject go, Vector3 pos)
+        {
+            target = go;
+            worldPosition = pos;
+        }
+    }
+
+    public static CursorCastHit CastCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (!Physics.Raycast(ray, out hit))
+            return null;
+
+        return new CursorCastHit(hit.collider.gameObject, hit.point);
+    }
+}
