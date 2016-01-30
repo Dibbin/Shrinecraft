50f9df357f3d8e7e5cfc3125dc6b43d7c2daa46e
Nathan <bryantnathanj@gmail.com>
2016-01-30 13:42:07 -0700

    god power hotbar beginning

---
+++ Assets/Scripts/GodPowerButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GodPowerButton : MonoBehaviour {
+
+    public string buttonId = "Fire";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void handleMouseClick()
+    {
+        Debug.Log(string.Format( buttonId +" button pushed!", buttonId));
+    }
+}
