c94bc79139f9746fb38fdc76ae0d0f01968d70cb
uberblah <miles.rufatlatre@colorado.edu>
2016-01-30 14:09:00 -0700

    added frames to follower text

--- Assets/Scripts/GodPowerButton.cs
+++ Assets/Scripts/GodPowerButton.cs
@@ -5,18 +5,8 @@ public class GodPowerButton : MonoBehaviour {
 
     public string buttonId = "Fire";
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     public void handleMouseClick()
     {
-        Debug.Log(string.Format( buttonId +" button pushed!", buttonId));
+        Debug.Log(string.Format( buttonId + " button pushed!", buttonId));
     }
 }
