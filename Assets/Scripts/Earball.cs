d85da84aa7d0ec59c3e1a06bf0a000c1e2daa895
uberblah <miles.rufatlatre@colorado.edu>
2016-01-30 12:45:12 -0700

    Earball

---
+++ Assets/Scripts/Earball.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Earball : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        CursorCaster.CursorCastHit hit = CursorCaster.CastCursor();
+        if(hit != null) gameObject.transform.position = hit.worldPosition;
+        gameObject.transform.rotation = Camera.main.gameObject.transform.rotation;
+        gameObject.transform.Rotate(new Vector3(0.0f, 180.0f, 0.0f));
+	}
+}
