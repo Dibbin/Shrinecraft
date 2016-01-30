5d20b1bc36f773ce6514eae459882007250edc6d
uberblah <miles.rufatlatre@colorado.edu>
2016-01-29 23:12:11 -0700

    skeletons

---
+++ Assets/Scripts/Shrine.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Shrine : MonoBehaviour {
+    public God god;
+    protected float nextEvent;
+    protected Queue<ShrineEvent> events;
+
+    public void PushEvent(ShrineEvent e)
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        nextEvent = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
