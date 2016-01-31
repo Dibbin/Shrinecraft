d6dd7bb4d958baf5688c746115d718c970e3789a
Nathan <bryantnathanj@gmail.com>
2016-01-31 15:16:03 -0700

    title menu and replay buttons

---
+++ Assets/Scripts/loadMainScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class loadMainScene : MonoBehaviour {
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
