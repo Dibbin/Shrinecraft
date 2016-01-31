1f15645b8a15cc2d93973e1e25d72e5e3c6225d4
Nick B <nick_beeson@yahoo.com>
2016-01-31 11:16:20 -0700

    Bug fixes, finish ruins, Working AI, Cleaner Scene

--- Assets/Scripts/GodController.cs
+++ Assets/Scripts/GodController.cs
@@ -7,6 +7,7 @@ public class GodController : MonoBehaviour {
     public God enemy;
     float energy;
     int choice;
+    List<Shrine> shrines;
     int followers;
     Vector3 randLoc;
     public Level level;
@@ -24,46 +25,59 @@ public class GodController : MonoBehaviour {
 
         };
 
-
+        setRandom();//changes its mind
+        run();//runs what it wants to do, if it has energy.
     }
 	void setRandom()
     {
         choice =  Random.Range(0,0);//set to length of dictionary
-
+        Invoke("setRandom", 2.0f);
     }
     void runPower(int i)
     {
         GodPower thePower=null;
-        switch (i)
-        {
-            case 0:
-                 thePower = godPowerDictionary["Fire"];
-                List<Shrine> shrines;
-                shrines = (level.GetShrinesByGod(playerGod));
-                int size = shrines.Count;
-                int sel = Random.Range(0, size);
-                thePower.usePowerOnShrine(shrines[sel], enemy);
-                break;
+       
+            switch (i)
+            {
+                case 0:
+                    thePower = godPowerDictionary["Fire"];
+                   
+                    shrines = (level.GetShrinesByGod(playerGod));
+                    int size = shrines.Count;
+                    int sel = Random.Range(0, size);
+                if (enemy.energy >= thePower.getEnergyCost()) 
+                {
+                    thePower.usePowerOnShrine(shrines[sel], enemy);
+                    shrines[sel].burner();
+                    enemy.energy -= thePower.getEnergyCost();
+                }
+                    break;
 
-            case 1:
-                 thePower = godPowerDictionary["SummonShrine"];
-                thePower.usePowerOnMap(randLoc, enemy);
+                case 1:
+                    thePower = godPowerDictionary["SummonShrine"];
+                    thePower.usePowerOnMap(randLoc, enemy);
 
-                break;
-            default:
+                    break;
+                default:
 
-                break;
-        }
+                    break;
+            }
+        
     }
     void run()
     {
-        runPower(choice);
+        
+            runPower(choice);
+        print("is shit broke?");
+            Invoke("run", 2.0f);
+
+        
     }
 	
 	void Update () {
-        Invoke("setRandom", 2.0f);//changes its mind
-        Invoke("run", 2.0f);     //runs what it wants to do, if it has energy.
 
+       
+        
 
 
 	}
