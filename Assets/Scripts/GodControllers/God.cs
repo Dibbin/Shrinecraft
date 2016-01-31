42a644202ba0d1a7da7b12e48b13c56afe91b611
Nick B <nick_beeson@yahoo.com>
2016-01-31 14:40:36 -0700

    added monk movement

---
+++ Assets/Scripts/GodControllers/God.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class God : MonoBehaviour {
+    public Level level;
+    public float energyPerFollowerPerSecond = 1.0f;
+    public int followers
+    {
+        get
+        {
+            int count = 0;
+            foreach(Shrine s in level.GetShrinesByGod(this))
+            {
+                count += s.followers;
+            }
+            return count;
+        }
+    }
+    public Slider energySlider;
+    public Text followersText;
+    public float energy
+    {
+        get { return energySlider.value; }
+        set { energySlider.value = value; }
+    }
+
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+        //regain energy depending on number of followers
+        energy += followers * energyPerFollowerPerSecond * Time.deltaTime;
+        followersText.text = followers.ToString();
+    }
+}
