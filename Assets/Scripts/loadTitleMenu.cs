d3600a8f51834c4a8ebb5e320426347655b033dc
Nathan <bryantnathanj@gmail.com>
2016-01-31 15:34:46 -0700

    menu sounds

---
+++ Assets/Scripts/loadTitleMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class loadTitleMenu : MonoBehaviour {
+
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene("TitleMenu");
+    }
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
