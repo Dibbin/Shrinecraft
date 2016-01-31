f0be3d413d4c8b468849dc58ce0df85e5b8bad2f
uberblah <miles.rufatlatre@colorado.edu>
2016-01-30 22:07:58 -0700

    initial sacrifice

---
+++ Assets/Scripts/GodPowerSacrifice.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GodPowerSacrifice : MonoBehaviour, GodPower {
+    public bool canUsePowerOnMap()
+    {
+        return false;
+    }
+
+    public bool canUsePowerOnShrine()
+    {
+        return true;
+    }
+
+    public float getEnergyCost()
+    {
+        return 10.0f;
+    }
+
+    public void usePowerOnMap(Vector3 point, God godUsingPower)
+    {
+        //nop;
+    }
+
+    public void usePowerOnShrine(Shrine s, God user)
+    {
+        s.followers--;
+        user.energy += 5;
+    }
+}
