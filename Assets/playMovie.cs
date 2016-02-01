518067aec7eeb841e874361169b3644c0d9cfe14
Nathan <bryantnathanj@gmail.com>
2016-01-31 17:31:25 -0700

    win/lose video scenes

---
+++ Assets/playMovie.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class playMovie : MonoBehaviour {
+
+    public bool loop = true;
+
+	// Use this for initialization
+	void Start () {
+
+        MovieTexture movie = GetComponent<RawImage>().texture as MovieTexture;
+        movie.loop = loop;
+
+        if (movie.isPlaying)
+        {
+            movie.Pause();
+        }
+        else {
+            movie.Play();
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
